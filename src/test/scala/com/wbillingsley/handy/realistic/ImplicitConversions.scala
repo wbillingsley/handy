8a61176358f059effef3ac6d25f601ae81f0e2df
William Billingsley <wbillingsley@cantab.net>
2013-02-01 12:18:17 +1000

    Added skeleton of how adding an enumerator method to RefMany
    This would be dependent on the Play framework iteratee classes, which
    I've not imported into test scope.

--- src/test/scala/com/wbillingsley/handy/realistic/ImplicitConversions.scala
+++ src/test/scala/com/wbillingsley/handy/realistic/ImplicitConversions.scala
@@ -26,6 +26,31 @@ object ImplicitResponses {
     
     p.future
   }
+  
+  
+
+  /**
+   * This is left here to give a hint as to how this is done, but I've not imported
+   * the ReactiveMongo classes into this project's test scope yet.
+   */
+  implicit class RefEnumerator[T](val refMany:RefMany[T]) {
+    
+    def enumerator = {
+      refMany match {
+        // case RefRMCursor(c) => c.enumerate
+        case _ => {
+          /*
+           * play.api.libs.iteratee.Concurrent.unicast[T] { channel =>	    	    
+	    	    for (r <- refMany) {
+	              channel.push(r)
+	            }
+	    	  }
+           */
+        }
+      }
+    }
+    
+  } 
 
   
 }
