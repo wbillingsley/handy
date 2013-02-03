b37768204ef3a135a3a1cffe75369dae8dde033b
William Billingsley <wbillingsley@cantab.net>
2013-02-03 22:58:52 +1000

    Added RefManyById

---
+++ src/main/scala/com/wbillingsley/handy/RefManyById.scala
@@ -0,0 +1,59 @@
+package com.wbillingsley.handy
+
+import Ref._
+
+/**
+ * Refers to many items by their ID.
+ */
+class RefManyById[T, K](val clazz : scala.Predef.Class[T], val rawIds: Seq[K]) extends RefMany[T] {
+  
+  def getIds[TT >: T, KK](implicit g:GetsId[TT, KK]) = {
+	for (i <- rawIds; c <- g.canonical(i)) yield c
+  }  
+  
+  def lookUp = RefManyById.lookUpMethod.lookup(this)  
+  
+  def first = Ref.fromOptionId(clazz, rawIds.headOption)
+  
+  def fetch = lookUp.fetch
+    
+  def isEmpty = rawIds.isEmpty
+  
+  def map[B](f: T => B) = lookUp.map(f)
+  
+  def flatMapMany[B](f: T => RefMany[B]) = lookUp.flatMapMany(f)
+
+  def flatMapOne[B](f: T => Ref[B]) = lookUp.flatMapOne(f)
+  
+  def foreach[U](f: T => U) { lookUp.foreach(f) }
+  
+  def withFilter(p: T => Boolean) = lookUp withFilter p
+  
+  def fold[B](initial: =>B)(each:(B, T) => B) = lookUp.fold(initial)(each)
+  
+  override def onReady[U](onSuccess: RefMany[T] => U, onNone: => U, onFail: Throwable => U) {
+    lookUp.onReady(onSuccess, onNone, onFail)
+  }  
+
+}
+
+object RefManyById {
+
+  trait LookUp {
+    def lookup[T](r: RefManyById[T, _]): RefMany[T]
+  }
+
+  /**
+   * The default lookup is not set. It would be possible to by default fetch each item in turn, but that
+   * is not preferred. 
+   */
+  val defaultLookUp = new LookUp {
+    def lookup[T](r: RefManyById[T, _]) = RefFailed(new UnsupportedOperationException("RefManyById.lookUpMethod not set"))
+  }
+
+  /**
+   * Set this method to define how RefByIds are looked up.
+   */
+  var lookUpMethod: LookUp = defaultLookUp
+
+}
