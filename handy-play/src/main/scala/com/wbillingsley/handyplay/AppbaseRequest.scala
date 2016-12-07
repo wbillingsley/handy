2f4bebf9a07f8f9d6ccb3c44b797d1f01945c988
William Billingsley <wbillingsley@cantab.net>
2016-12-07 22:49:59 +1100

    Removed DataAction from handy-play, pending redesign of that part
    handy-play hasn't been used in a while and was broken. This commit removes the broken DataAction (that needs redesigning anyway), and adjusts UserAction so it works with Play 2.5
    
    A future commit will add in RefMany -> reactive streams chunked response functionality, but that's not included in this one.

--- handy-play/src/main/scala/com/wbillingsley/handyplay/AppbaseRequest.scala
+++ handy-play/src/main/scala/com/wbillingsley/handyplay/AppbaseRequest.scala
@@ -1,20 +1,62 @@
 package com.wbillingsley.handyplay
 
-import play.api.mvc.{WrappedRequest, Request}
-import com.wbillingsley.handy.Approval
+import java.util.NoSuchElementException
+import play.api.libs.json.Json
+import play.api.mvc._
+import com.wbillingsley.handy.{Ref, Approval}
+import Ref._
 
+import scala.concurrent.Future
 
-class AppbaseRequest[A, U](request:Request[A])(implicit ufr:UserFromRequest[U]) extends WrappedRequest(request) {
+case class Appbase[U](
+  ufr:UserFromRequest[U],
+  errorPF:PartialFunction[Throwable, Future[Result]] = {
+    case e:NoSuchElementException => Future.successful(Results.NotFound(Json.obj("error" -> e.getMessage)))
+  }
+) {
 
-  lazy val user = ufr.user(request)
-  
-  lazy val approval = new Approval(user)
-  
-  val sessionKey = request.session.get("sessionKey").getOrElse(AppbaseRequest.newSessionKey)
-}
+  class AppbaseRequest[A](request:Request[A]) extends WrappedRequest(request) {
 
-object AppbaseRequest {
-  
-  def newSessionKey = java.util.UUID.randomUUID.toString
-  
+    lazy val user = ufr.user(request)
+
+    lazy val approval = new Approval(user)
+
+    val sessionKey = request.session.get("sessionKey").getOrElse(AppbaseRequest.newSessionKey)
+  }
+
+  object AppbaseRequest {
+    def newSessionKey = java.util.UUID.randomUUID.toString
+  }
+
+  object UserAction extends ActionBuilder[AppbaseRequest] with ActionTransformer[Request, AppbaseRequest] {
+
+    import play.api.libs.concurrent.Execution.Implicits._
+
+    def transform[A](request: Request[A]) = Future.successful {
+      new AppbaseRequest(request)
+    }
+
+    def invokeBlock[A](request: Request[A])(block:AppbaseRequest[A] => Future[Result]) = {
+      (for {
+        r <- transform(request)
+        res <- block(r)
+      } yield {
+        res
+          .withHeaders(
+            "Cache-Control" -> "no-cache, no-store, must-revalidate", "Expires" -> "0", "Pragma" -> ""
+          )
+          .addingToSession("sessionKey" -> r.sessionKey)(r)
+      }).recoverWith(errorPF)
+
+    }
+
+    implicit def rtf(rr:Ref[Result]):Future[Result] = {
+      rr
+        .orIfNone(Results.NotFound(Json.obj("error" -> "Not found")).itself)
+        .toFuture
+        .recoverWith(errorPF)
+    }
+
+  }
+
 }
